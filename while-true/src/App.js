1cb98fc4a737a3c230ac9ab38115385db3238741
Jett Seale <sealee@oregonstate.edu>
2020-06-21 11:59:55 -0700

    rewrote app.js and flashcard.js for easier access to fields

--- while-true/src/App.js
+++ while-true/src/App.js
@@ -4,18 +4,37 @@ import { Button } from '@material-ui/core';
 import FlashCard from './FlashCard';
 import './App.css';
 
-
-function App() {
+const App = () => {
+  const [questionVals, setQuestionVals] = React.useState({});
+  const [answerVals, setAnswerVals] = React.useState({});
   const nums = [0, 1, 2, 3, 4];
+
+  const handleFieldChange = (fieldId, questionVal, answerVal) => {
+    if (questionVal !== '') setQuestionVals({...questionVals, [fieldId]: questionVal});
+    if (answerVal !== '') setAnswerVals({...answerVals, [fieldId]: answerVal});
+  };
+
+  const flashCards = nums.map(i => (
+    <FlashCard
+      key={i}
+      id={i}
+      onChange={handleFieldChange}
+      qValues={questionVals[i]}
+      aValues={answerVals[i]}
+    />
+  ));
+  
+  //Try typing in any field and see the change in real-time:
+  console.log(questionVals);
+  console.log(answerVals); 
+
   return (
     <div className="app-wrapper">
       <div className="app">
         <header className="app-header">
           Study Hub
         </header>
-        {
-          nums.map((i) => { return <div className="flash-card-wrapper" key={i}> <FlashCard id={i}/> </div> })
-        }
+        { flashCards }
         <div className="button-wrapper">
           <Button id="create-deck-button" variant="contained" color="primary">
             Create Deck
--- while-true/src/FlashCard.js
+++ while-true/src/FlashCard.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { TextField } from '@material-ui/core';
 
-function FlashCard(id) {
-    const [question, setQuestion] = React.useState('');
-    const [answer, setAnswer] = React.useState('');
+function FlashCard({qValue, aValue, onChange, id}) {
 
-    const handleQuestionChange = (event) => {
-        setQuestion(event.target.value);
-        console.log(`event.target.value: ${event.target.value}`);
-        console.log(`question: ${question}`);
+    const handleQuestionChange = event => {
+        const text = event.target.value;
+        onChange(id, text, '');
     };
-    const handleAnswerChange = (event) => {
-        setAnswer(event.target.value);
-        console.log(`event.target.value:" ${event.target.value}`);
-        console.log(`answer: ${answer}`);
+
+    const handleAnswerChange = event => {
+        const text = event.target.value;
+        onChange(id, '', text);
     };
 
     return (
         <>
-            <div className="flash-card-header">Card #{id.id + 1}</div>
-            <div className="flash-card" id={id.id}>
-                <TextField id="question-input" label={`Enter question ${id.id + 1}`} variant="outlined" style={{gridArea: "left"}} value={question} onChange={handleQuestionChange}/>
-                <TextField id="answer-input" label={`Enter answer ${id.id + 1}`} variant="outlined" style={{gridArea: "right"}} value={answer} onChange={handleAnswerChange}/>
+            <div className="flash-card-header">Card #{id + 1}</div>
+            <div className="flash-card" id={id}>
+                <TextField id="question-input" label={`Enter question ${id + 1}`} variant="outlined" style={{gridArea: "left"}} value={qValue} onChange={handleQuestionChange}/>
+                <TextField id="answer-input" label={`Enter answer ${id + 1}`} variant="outlined" style={{gridArea: "right"}} value={aValue} onChange={handleAnswerChange}/>
             </div>
         </>
     );
